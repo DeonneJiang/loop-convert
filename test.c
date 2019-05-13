d14994c834931082b48fcbb244861192962a1847
Ale <deonnejiang@163.com>
2019-05-13 23:59:47 +0800

    'update'

--- test.c
+++ test.c
@@ -5,22 +5,8 @@ int b;
 
 int c = 10;
 
-
-
-int function1(){ int a = 1; return a;}
-
-int function2(){ int a = 2; return a;}
-
-
 int main(){
 
-    int d;
-
-    int g = 100;
-
-    int a[100] = {0};
-    function1();
-    function2();
     return 0;
     
 }
